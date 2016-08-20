ab99b7d3ef9bec5057ce4df6060231a4186d60c4
rvzm <rvxm@outlook.com>
2016-08-20 08:23:43 -0800

    initial commit

---
+++ zconf-settings.tcl
@@ -0,0 +1,14 @@
+# zconf-settings.tcl
+
+namespace eval zconf {
+	namespace eval settings {
+		variable pubtrig "&";
+		variable zchan "#znc";
+		variable url "placeholder";
+		variable version "0.3";
+		variable pass "placeholder";
+		variable zncpass "placeholder";
+
+	}
+}
+putlog "zConf - Settings Loaded";
