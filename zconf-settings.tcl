a67860f64b65e9550bcf6fb4e5a9ece615dff06c
Connor Angell <rvzm@protonmail.com>
2016-12-09 11:58:09 -0900

    version update

--- zconf-settings.tcl
+++ zconf-settings.tcl
@@ -20,7 +20,7 @@ namespace eval zconf {
 		variable url "placeholder";
 		variable weblink $url;
 		variable irclink "placeholder";
-		variable version "0.7.7";
+		variable version "0.7.8-dev";
 		variable pass "placeholder";
 		variable zncpass "placeholder";
 		variable passlen "23";
